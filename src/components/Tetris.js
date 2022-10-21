e3e974c4c8cbfe784e108f6a324a24d64b871953
Pooja <poojag23.pg@gmail.com>
2022-10-21 09:47:17 -0400

    Added game status

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,10 +1,18 @@
 import "./Tetris.css";
 import Board from "./Board";
+import GameStats from "./GameStats";
 import { useBoard } from "../hooks/useBoard";
+import {useGameStats} from "../hooks/useGameStats.js";
 
 const Tetris = ({ rows, columns, setGameOver }) => {
+  const [gameStats, addLinesCleared]=useGameStats();
   const [board, setBoard] = useBoard({ rows, columns });
-  return (<Board board={board} />);
+  return (
+    <div className='Tetris'>
+      <Board board={board} />
+      <GameStats gameStats={gameStats} />
+    </div>
+  );
 };
 
 export default Tetris;
