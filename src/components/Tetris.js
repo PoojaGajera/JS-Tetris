b19d17d1ca3bc27728df35ffb7c7d9950caab9c4
Pooja <poojag23.pg@gmail.com>
2022-10-21 10:14:11 -0400

    Preview and Player Data

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,16 +1,20 @@
 import "./Tetris.css";
 import Board from "./Board";
 import GameStats from "./GameStats";
+import Previews from "./Previews";
 import { useBoard } from "../hooks/useBoard";
-import {useGameStats} from "../hooks/useGameStats.js";
+import { useGameStats } from "../hooks/useGameStats.js";
+import { usePlayer } from "../hooks/usePlayer.js";
 
 const Tetris = ({ rows, columns, setGameOver }) => {
-  const [gameStats, addLinesCleared]=useGameStats();
+  const [gameStats, addLinesCleared] = useGameStats();
   const [board, setBoard] = useBoard({ rows, columns });
+  const [player, setPlayer, resetPlayer] = usePlayer();
   return (
     <div className='Tetris'>
       <Board board={board} />
       <GameStats gameStats={gameStats} />
+      <Previews tetrominoes={player.tetrominoes} />
     </div>
   );
 };
--- src/hooks/useGameStats.js
+++ src/hooks/useGameStats.js
@@ -4,12 +4,12 @@ const buildGameStats = () => ({
   level: 1,
   linesCompleted: 0,
   linesPerLevel: 10,
-  points: 0
+  points: 0,
 });
 
-export const useGameStats=()=>{
-    const [gameStats, setGameStats] = useState(buildGameStats());
-    const addLinesCleared = useCallback(()=>{},[]);
+export const useGameStats = () => {
+  const [gameStats, setGameStats] = useState(buildGameStats());
+  const addLinesCleared = useCallback(() => {}, []);
 
-    return [gameStats, addLinesCleared]
-}
+  return [gameStats, addLinesCleared];
+};
