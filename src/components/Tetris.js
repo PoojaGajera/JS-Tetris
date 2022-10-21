0a10bd6886f9d16a023b4e0ba307e4d5d7c311e7
Pooja <poojag23.pg@gmail.com>
2022-10-21 10:35:38 -0400

    Rendered Player

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -8,8 +8,15 @@ import { usePlayer } from "../hooks/usePlayer.js";
 
 const Tetris = ({ rows, columns, setGameOver }) => {
   const [gameStats, addLinesCleared] = useGameStats();
-  const [board, setBoard] = useBoard({ rows, columns });
   const [player, setPlayer, resetPlayer] = usePlayer();
+  const [board, setBoard] = useBoard({
+    rows,
+    columns,
+    player,
+    resetPlayer,
+    addLinesCleared,
+  });
+
   return (
     <div className='Tetris'>
       <Board board={board} />
